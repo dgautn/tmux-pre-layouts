a28e3c4a003de9e3991b3a5b845fdab109876374
gustavo <dariogustavoalbarran@gmail.com>
2021-08-10 18:44:15 -0300

    en master ++++ variables

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 #bind_key='@'
-path_layouts="$HOME/.tmux/pre-layouts"
+#path_layouts="$HOME/.tmux/pre-layouts"
 bind_key="@pre-layouts-key"
-#path_layouts="@pre-layout-dir"
+path_layouts="@pre-layout-dir"
